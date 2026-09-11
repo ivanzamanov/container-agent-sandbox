18a559f3471cda0c3b9086faf869fa316d5e9ce3
Ivan Zamanov <ivan.zamanov@investing.one>
2026-09-11 15:13:34 +0300

    fix: zellij config

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,8 +5,6 @@ echo "Started sandbox"
 
 source ~/.bashrc
 
-zellij web --port 8082 --daemonize --ip 127.0.0.2
-
-socat TCP-LISTEN:8082,bind=0.0.0.0,fork TCP:127.0.0.2:8082 &
+zellij web --daemonize
 
 exec opencode web --port 4444 --hostname 0.0.0.0
